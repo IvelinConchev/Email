4fb2ca27b6539dc632438eb71b7d1eb870cf79dc
IvelinConchev <ivelin_conchev@abv.bg>
2019-12-14 14:05:17 +0200

    Update EmailTest.php

--- Tests/EmailTest.php
+++ Tests/EmailTest.php
@@ -9,7 +9,7 @@ final class EmailTest extends TestCase
     {
         $this->assertInstanceOf(
             Email::class,
-            Email::fromString('user@')
+            Email::fromString('user@example.com')
         );
     }
 
